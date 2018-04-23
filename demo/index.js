111159448f89d5f2aad118ab2e8853cbde3b9af8
Rubén Martín Lozano <ruben.martin.lozano@gmail.com>
2018-04-23 18:09:19 +0200

    test

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react'
 import ReactDOM from 'react-dom'
 import Tab from '../src/index.jsx'
 
-const tab = [
+const ukuleleTab = [
   [false, 0, false, false],
   [false, 2, false, false],
   [false, false, 0, false],
@@ -20,9 +20,28 @@ const tab = [
   [false, 0, false, false]
 ]
 
+const guitarTab = [
+  [false, false, 0, false, false, false],
+  [false, false, 2, false, false, false],
+  [false, false, false, 0, false, false],
+  [false, false, false, 1, false, false],
+  [false, false, false, 3, false, false],
+  [false, false, false, false, 0, false],
+  [false, false, false, false, 2, false],
+  [false, false, false, false, 3, false],
+  [false, false, false, false, 2, false],
+  [false, false, false, false, 0, false],
+  [false, false, false, 3, false, false],
+  [false, false, false, 1, false, false],
+  [false, false, false, 0, false, false],
+  [false, false, 2, false, false, false],
+  [false, false, 0, false, false, false]
+]
+
 const demo = (
   <Fragment>
-    <Tab tab={tab} />
+    <Tab tab={ukuleleTab} />
+    <Tab tab={guitarTab} />
   </Fragment>
 )
 
